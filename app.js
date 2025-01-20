9db4c1f8ee3443b23764e303c767b83746144488
Casper <casperdennijs+github@proton.me>
2025-01-21 00:22:01 +0100

    Initial commit - Version 1

---
+++ app.js
@@ -0,0 +1,72 @@
+const body = document.querySelector("body")
+const answer = document.querySelector(".answer");
+const text = document.querySelector(".text")
+
+navigator.permissions.query({ name: "geolocation" }).then((result) => {
+    if (result.state === "granted") {
+        report(result.state);
+        getLocationPrompt();
+    } else if (result.state === "prompt") {
+        report(result.state);
+        getLocationPrompt();
+    } else if (result.state === "denied") {
+        report(result.state);
+        document.title = "Is het droog? - Geen toestemming";
+        answer.textContent = "Geen toestemming";
+        text.textContent = "We hebben je locatie nodig om te bepalen of het droog is.";
+        body.classList.toggle("error");
+    }
+    result.addEventListener("change", () => {
+        report(result.state);
+    });
+});
+
+function report(state) {
+    console.log(`Permission ${state}`);
+}
+
+function getLocationPrompt() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        console.log("Browser doesn't support the Geolocation API");
+    }
+}
+
+function success(position) {
+    getData(position.coords.latitude, position.coords.longitude);
+}
+
+function error() {
+    document.title = "Is het droog? - Geen toestemming";
+    answer.textContent = "Geen toestemming";
+    text.textContent = "We hebben je locatie nodig om te bepalen of het droog is.";
+    body.classList.toggle("error");
+}
+
+async function getData(latitude, longitude) {
+    const url = `https://api.open-meteo.com/v1/forecast?models=knmi_seamless&latitude=${latitude}&longitude=${longitude}&current=rain`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+    
+        const json = await response.json();
+        console.log("Rain: " + json.current.rain)
+        if (json.current.rain != 0) {
+            document.title = "Is het droog? - Nee";
+            answer.textContent = "Nee";
+            text.textContent = "Het is momenteel niet droog.";
+            body.classList.toggle("wet");
+        } else {
+            document.title = "Is het droog? - Ja";
+            answer.textContent = "Ja";
+            text.textContent = "Het is momenteel droog.";
+            body.classList.toggle("dry");
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+  
